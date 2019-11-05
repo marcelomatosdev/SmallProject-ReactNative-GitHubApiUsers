badc50abf65876cec7a538bb02edc577d3eb1243
marcelomatosdev <marcelomatos.dev@gmail.com>
2019-11-04 22:02:32 -0600

    Working Version with Reactotron v2

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
-import { registerRootComponent } from "expo";
-import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+// import { registerRootComponent } from 'expo';
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+import './config/ReactotronConfig';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#123456",
-    alignItems: "center",
-    justifyContent: "center"
-  }
+   container: {
+      flex: 1,
+      backgroundColor: '#123456',
+      alignItems: 'center',
+      justifyContent: 'center',
+   },
 });
 
+console.tron.warn('Hello World5');
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Agora a parada ficou seria!</Text>
-      <Text>Abaixo7</Text>
-    </View>
-  );
+   return (
+      <View style={styles.container}>
+         <Text>Agora a parada ficou seria!</Text>
+         <Text>Abaixo8</Text>
+      </View>
+   );
 }
